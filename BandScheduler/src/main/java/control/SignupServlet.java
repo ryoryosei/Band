115d9062ded282f5657aa8a6bcbdd775a4685f58
ryoryosei <nodaryousei@icloud.com>
2024-07-20 22:26:02 +0900

    files upload

---
+++ BandScheduler/src/main/java/control/SignupServlet.java
@@ -0,0 +1,85 @@
+package control;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignupServlet
+ */
+@WebServlet("/signup-servlet")
+public class SignupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		String userId = request.getParameter("userId");
+		String password = request.getParameter("password");
+		
+		Connection con = null;
+		String url = null;
+	       try {
+	    	 Class.forName("com.mysql.cj.jdbc.Driver");
+	         con = DriverManager.getConnection("jdbc:mysql://localhost/java", "root","Noda37458@@");
+	         //
+	         System.out.println("database connected");
+	         String sql = "insert into login (name,password) values (?,?);";
+	         PreparedStatement st = con.prepareStatement(sql);
+	        st.setString(1,userId);
+	        st.setString(2, password);
+	        //
+	        
+	         st.executeUpdate();
+	         url = "Login.jsp";
+	       
+	       } catch (ClassNotFoundException e) {
+		    	//   
+	    	   System.out.println("Driver not found");
+		    	   e.printStackTrace();
+		    	   
+	       } catch (SQLException ex) {
+	    	   ex.printStackTrace();
+	    	   request.setAttribute("errorMsg", "データベース接続に失敗しました。");	
+	    	   url = "Login.jsp";
+	       }
+	       
+	       RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+			dispatcher.forward(request, response);
+	}
+		
+		
+		
+}
+	
+	
+
